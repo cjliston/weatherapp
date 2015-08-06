97a93793555923d105a8398d07651349fc3408f2
cjliston <gyt0312@gmail.com>
2015-08-06 07:20:44 -0700

    Added forecast functionality and loading spinner

--- weather.js
+++ weather.js
@@ -1,16 +1,27 @@
 
- $('document').ready(function(){
- $('form').submit(function (evt) {
+$('document').ready(function(){
+//weather converter
+
+function dateConverter(date){
+  var fullDate = new Date (1000*date);
+  var stringDate = fullDate.toUTCString();
+  var newDate = stringDate.substring(0,11);
+  return newDate;
+};
+
+$('#showMore').hide();
+$('form').submit(function (evt) {
     evt.preventDefault();
     $('#welcome').hide();
-    // the AJAX part
+    // Get weather data
     var weatherAPI = "//api.openweathermap.org/data/2.5/weather";
     var city = $(search).val();
     var weatherOptions = {
       q: city,
       APPID: "67e821ad4a722dee24b61f0077ebf404",
-units:"imperial"
+      units:"imperial"
     };
+    //create HTML for weather display
     function displayTemperature(data) {
       var weatherHTML ="<div id='weatherData'><div class='shout city'>"+data.name+","+data.sys.country+"</div><p>Temperature: "+data.main.temp+" degrees</p><p>Wind: "+data.wind.speed+" MPH</p>";
       if(data.main.temp>=100){
@@ -24,10 +35,45 @@ units:"imperial"
       }else{
         weatherHTML += '<div class="shout">TOO DAMN COLD. STAY THE FUCK INSIDE</div><div><img src="img/cold.gif"></div></div>'
       }
-      $('#weather').html(weatherHTML)}
+      $('#weather').html(weatherHTML)
+      $('#showMore').show();
+      $('#forecastsDiv').hide();
+    }
 
     $.getJSON(weatherAPI, weatherOptions, displayTemperature);
+//start show more
+$('#showMoreBtn').click(function (evt){
+  evt.preventDefault();
+  $('#showMore').hide();
+  $('#spinner').show();
+  // Get weather data
+    var forecastAPI = "//api.openweathermap.org/data/2.5/forecast/daily";
+    var city = $(search).val();
+    var forecastOptions = {
+      q: city,
+      APPID: "67e821ad4a722dee24b61f0077ebf404",
+      units:"imperial",
+      cnt:7,
+      mode:'JSON',
+    };
+    //create HTML for weather display
+    function displayForecast(data) {
+      var forecastHTML = '<div id="forecasts">'
+      $.each(data.list,function(i,day) {
+        forecastHTML += '<div class="forecast"><p>'+ dateConverter(day.dt) + '</p><p>High: '+day.temp.max+'</p><p>Low:'+ day.temp.min+'</p></div>'
+      }); // end each
+      forecastHTML +='</div>'
+      $('#spinner').hide();
+      $('#forecastsDiv').html(forecastHTML);
+      $('#forecastsDiv').show();
+    }
 
+    $.getJSON(forecastAPI, forecastOptions, displayForecast);
+
+});//end click for show more
   }); // end click
 
+
 }); // end ready
+
+
